0514b28bec463f997de64edbaebdca1747fda0b5
Huynh Ngoc Minh Tri <minhtri191195@gmail.com>
2018-03-09 08:38:57 +0700

    Fix convetion in blade and some thing wrong

--- resources/views/users/master.blade.php
+++ resources/views/users/master.blade.php
@@ -1,76 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-<title>Motel My Lan | Best hotel in DN</title>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<title>Best hotel in Da Nang</title>
 
-<!-- Google fonts -->
-<link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800|Old+Standard+TT' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800' rel='stylesheet' type='text/css'>
+		<!-- Google fonts -->
+		<link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800|Old+Standard+TT' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800' rel='stylesheet' type='text/css'>
 
-<!-- font awesome -->
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<!-- font awesome -->
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
+		<!-- bootstrap -->
+		<link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
 
-<!-- bootstrap -->
-<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+		<!-- uniform -->
+		<link type="text/css" rel="stylesheet" href="{{ asset('assets/uniform/css/uniform.default.min.css') }}">
 
-<!-- uniform -->
-<link type="text/css" rel="stylesheet" href="assets/uniform/css/uniform.default.min.css" />
+		<!-- animate.css -->
+		<link rel="stylesheet" href="{{ asset('assets/wow/animate.css') }}">
 
-<!-- animate.css -->
-<link rel="stylesheet" href="assets/wow/animate.css" />
+		<!-- gallery -->
+		<link rel="stylesheet" href="{{ asset('assets/gallery/blueimp-gallery.min.css') }}">
 
+		<!-- favicon -->
+		<link rel="shortcut icon" href="images/logo2.png" type="image/x-icon">
+		<link rel="icon" href="images/logo2.png" type="image/x-icon">
 
-<!-- gallery -->
-<link rel="stylesheet" href="assets/gallery/blueimp-gallery.min.css">
+		<link rel="stylesheet" href="{{ asset('assets/style.css') }}">
+	</head>
+	<body id="home">
+		<form class="navbar-form navbar-left newsletter" role="search">
+			<div class="form-group">
+				<input type="text" class="form-control" placeholder="Enter Your Email Id Here">
+			</div>
+			<button type="submit" class="btn btn-inverse">Subscribe</button>
+		</form>
+		@include('users.blocks.menu')
+		@yield('content')
+		@include('users.blocks.footer')
 
+		<script src="{{ asset('assets/jquery.js') }}"></script>
 
-<!-- favicon -->
-<link rel="shortcut icon" href="images/logo2.png" type="image/x-icon">
-<link rel="icon" href="images/logo2.png" type="image/x-icon">
+		<!-- wow script -->
+		<script src="{{ asset('/assets/wow/wow.min.js') }}"></script>
 
-<link rel="stylesheet" href="assets/style.css">
+		<!-- uniform -->
+		<script src="{{ asset('assets/uniform/js/jquery.uniform.js') }}"></script>
 
-</head>
+		<!-- boostrap -->
+		<script src="{{ asset('assets/bootstrap/js/bootstrap.js') }}" type="text/javascript" ></script>
 
-<body id="home">
+		<!-- jquery mobile -->
+		<script src="{{ asset('assets/mobile/touchSwipe.min.js') }}"></script>
 
-  <form class="navbar-form navbar-left newsletter" role="search">
-        <div class="form-group">
-        	<input type="text" class="form-control" placeholder="Enter Your Email Id Here">
-        </div>
-        <button type="submit" class="btn btn-inverse">Subscribe</button>
-    </form>
-    @include('users.blocks.menu')
-  @yield('content')
-  @include('users.blocks.footer')
+		<!-- jquery mobile -->
+		<script src="{{ asset('assets/respond/respond.js') }}"></script>
 
-<script src="assets/jquery.js"></script>
+		<!-- gallery -->
+		<script src="{{ asset('assets/gallery/jquery.blueimp-gallery.min.js') }}"></script>
 
-<!-- wow script -->
-<script src="assets/wow/wow.min.js"></script>
-
-<!-- uniform -->
-<script src="assets/uniform/js/jquery.uniform.js"></script>
-
-
-<!-- boostrap -->
-<script src="assets/bootstrap/js/bootstrap.js" type="text/javascript" ></script>
-
-<!-- jquery mobile -->
-<script src="assets/mobile/touchSwipe.min.js"></script>
-
-<!-- jquery mobile -->
-<script src="assets/respond/respond.js"></script>
-
-<!-- gallery -->
-<script src="assets/gallery/jquery.blueimp-gallery.min.js"></script>
-
-
-<!-- custom script -->
-<script src="assets/script.js"></script>
-
-</body>
+		<!-- custom script -->
+		<script src="{{ asset('assets/script.js') }}"></script>
+	</body>
 </html>
--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,58 +1,56 @@
 @component('mail::message')
-{{-- Greeting --}}
-@if (! empty($greeting))
-# {{ $greeting }}
-@else
-@if ($level == 'error')
-# Whoops!
-@else
-# Hello!
-@endif
-@endif
+    {{-- Greeting --}}
+    @if (! empty($greeting))
+        # {{ $greeting }}
+    @else
+        @if ($level == 'error')
+            # Whoops!
+        @else
+            # Hello!
+        @endif
+    @endif
 
-{{-- Intro Lines --}}
-@foreach ($introLines as $line)
-{{ $line }}
+    {{-- Intro Lines --}}
+    @foreach ($introLines as $line)
+        {{ $line }}
+    @endforeach
 
-@endforeach
+    {{-- Action Button --}}
+    @if (isset($actionText))
+        <?php
+            switch ($level) {
+                case 'success':
+                    $color = 'green';
+                    break;
+                case 'error':
+                    $color = 'red';
+                    break;
+                default:
+                    $color = 'blue';
+            }
+        ?>
+        @component('mail::button', ['url' => $actionUrl, 'color' => $color])
+            {{ $actionText }}
+        @endcomponent
+    @endif
 
-{{-- Action Button --}}
-@if (isset($actionText))
-<?php
-    switch ($level) {
-        case 'success':
-            $color = 'green';
-            break;
-        case 'error':
-            $color = 'red';
-            break;
-        default:
-            $color = 'blue';
-    }
-?>
-@component('mail::button', ['url' => $actionUrl, 'color' => $color])
-{{ $actionText }}
-@endcomponent
-@endif
+    {{-- Outro Lines --}}
+    @foreach ($outroLines as $line)
+        {{ $line }}
+    @endforeach
 
-{{-- Outro Lines --}}
-@foreach ($outroLines as $line)
-{{ $line }}
+    <!-- Salutation -->
+    @if (! empty($salutation))
+        {{ $salutation }}
+    @else
+        Regards,<br>{{ config('app.name') }}
+    @endif
 
-@endforeach
-
-<!-- Salutation -->
-@if (! empty($salutation))
-{{ $salutation }}
-@else
-Regards,<br>{{ config('app.name') }}
-@endif
-
-<!-- Subcopy -->
-@if (isset($actionText))
-@component('mail::subcopy')
-If you’re having trouble clicking the "{{ $actionText }}" button, copy and paste the URL below
-into your web browser: [{{ $actionUrl }}]({{ $actionUrl }})
-@endcomponent
-@endif
+    <!-- Subcopy -->
+    @if (isset($actionText))
+        @component('mail::subcopy')
+        If you’re having trouble clicking the "{{ $actionText }}" button, copy and paste the URL below
+        into your web browser: [{{ $actionUrl }}]({{ $actionUrl }})
+        @endcomponent
+    @endif
 @endcomponent
